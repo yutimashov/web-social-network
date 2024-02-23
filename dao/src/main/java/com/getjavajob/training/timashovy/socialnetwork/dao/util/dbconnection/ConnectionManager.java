c5ca31b94a44ec9db9db6299bda64a8563d9545d
yutimashov <timashov.yuriy@gmail.com>
2024-02-24 00:10:37 +0300

    add dao util classes for establishing db connection

---
+++ dao/src/main/java/com/getjavajob/training/timashovy/socialnetwork/dao/util/dbconnection/ConnectionManager.java
@@ -0,0 +1,95 @@
+package com.getjavajob.training.timashovy.socialnetwork.dao.util.dbconnection;
+
+import com.getjavajob.training.timashovy.socialnetwork.dao.util.exceptions.DaoException;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+import static com.getjavajob.training.timashovy.socialnetwork.dao.util.dbconnection.PropertiesUtil.get;
+import static java.lang.Class.forName;
+import static java.lang.Integer.parseInt;
+
+/**
+ * Class responsible for creation connections to DB.
+ * It provides only one public method for clients - {@link ConnectionManager#getConnection()}.
+ * All the work with connections to DB is organized via connection pool.
+ *
+ * @author Yuriy Timashov
+ * @since 14.01.2023
+ */
+public final class ConnectionManager {
+
+    private static final String URL_KEY = "db.url";
+    private static final String LOGIN_KEY = "db.login";
+    private static final String PASSWORD_KEY = "db.password";
+    private static final String POOL_SIZE_KEY = "db.pool.size";
+    private static BlockingQueue<Connection> connectionPool;
+    private static final Integer DEFAULT_POOL_SIZE = 10;
+
+    static {
+        loadDriver();
+        initializeConnectionPool();
+    }
+
+    /**
+     * Class is not considered to have any instances.
+     * It provides only one static util method.
+     *
+     * @throws AssertionError trying to create instance
+     */
+    private ConnectionManager() {
+        throw new AssertionError();
+    }
+
+    private static void initializeConnectionPool() {
+        String poolSize = get(POOL_SIZE_KEY);
+        int size = poolSize == null ? DEFAULT_POOL_SIZE : parseInt(poolSize);
+        connectionPool = new ArrayBlockingQueue<>(size);
+        for (int i = 0; i < size; i++) {
+            connectionPool.add(createConnection());
+        }
+    }
+
+    /**
+     * Public method for clients to work with connections to DB, using connection pool.
+     *
+     * @return connection to DB
+     */
+    public static Connection getConnection() {
+        try {
+            return connectionPool.take();
+        } catch (InterruptedException e) {
+            throw new DaoException("Cannot establish connection to db");
+        }
+    }
+
+    private static void loadDriver() {
+        try {
+            forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new DaoException("Cannot load db driver");
+        }
+    }
+
+    /**
+     * This method is private in order to avoid any custom creation of connection to DB.
+     * It only allows to work with connection through connection pool.
+     * So this method is used to create connections to DB with connection pool.
+     * This method uses fake connection - instance of ConnectionWrapper in order to
+     * return used connection to connection pool, rather than just close it.
+     *
+     * @return connection instance to database, based on config extracted data
+     */
+    private static Connection createConnection() {
+        try {
+            Connection realConnection = DriverManager.getConnection(get(URL_KEY), get(LOGIN_KEY), get(PASSWORD_KEY));
+            return new ConnectionWrapper(realConnection, connectionPool);
+        } catch (SQLException e) {
+            throw new DaoException("Cannot create connection to db");
+        }
+    }
+
+}
