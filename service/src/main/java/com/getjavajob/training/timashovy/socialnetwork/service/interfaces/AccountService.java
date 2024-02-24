e468b74876c7e6e08a75a10236cc4045dac45441
yutimashov <timashov.yuriy@gmail.com>
2024-02-24 11:54:16 +0300

    add AccountService interface & update pom.xml adding dependency for Account entity class

---
+++ service/src/main/java/com/getjavajob/training/timashovy/socialnetwork/service/interfaces/AccountService.java
@@ -0,0 +1,27 @@
+package com.getjavajob.training.timashovy.socialnetwork.service.interfaces;
+
+import com.getjavajob.training.timashovy.socialnetwork.common.Account;
+
+import java.util.List;
+
+/**
+ * Interface contains all methods for organizing logic in communication with {@link Account} entity in application.
+ *
+ */
+public interface AccountService {
+
+    Long createAccount(Account account);
+
+    boolean updateAccount(Long accountId, Account updatedAccount);
+
+    boolean deleteAccount(Long accountId);
+
+    List<Account> getAllAccounts();
+
+    boolean addFriend(Long accountId, Long friendId);
+
+    boolean deleteFriend(Long accountId, Long friendId);
+
+    List<Account> getFriends(Long accountId);
+
+}
