05f1072b015fd35ff65bc0eefad85278a2e0f809
yutimashov <timashov.yuriy@gmail.com>
2024-03-17 01:15:07 +0300

    add basic logout functionality

---
+++ web/src/main/java/com/getjavajob/training/timashovy/socialnetwork/web/servlets/LogoutServlet.java
@@ -0,0 +1,17 @@
+package com.getjavajob.training.timashovy.socialnetwork.web.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class LogoutServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getSession().invalidate();
+        resp.sendRedirect("/login");
+    }
+
+}
--- web/src/main/webapp/WEB-INF/jsp/header.jsp
+++ web/src/main/webapp/WEB-INF/jsp/header.jsp
@@ -1,5 +1,11 @@
 <%@ page contentType="text/html;charset=UTF-8" %>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
-<c:if test="${sessionScope.account != null}">
+<c:if test="${not empty sessionScope.account}">
+    <div>
+        <form action="${pageContext.request.contextPath}/logout" method="POST">
+            <button type="submit">Logout</button>
+        </form>
+    </div>
     <h2>Пользователь: ${sessionScope.account.firstName} ${sessionScope.account.lastName}</h2>
+    <hr>
 </c:if>
