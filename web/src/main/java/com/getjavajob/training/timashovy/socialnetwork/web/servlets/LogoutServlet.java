154ca3a2df0e452454920d7027e4d72998add1b0
yutimashov <timashov.yuriy@gmail.com>
2024-03-19 21:26:28 +0300

    fix logout servlet functionality

--- web/src/main/java/com/getjavajob/training/timashovy/socialnetwork/web/servlets/LogoutServlet.java
+++ web/src/main/java/com/getjavajob/training/timashovy/socialnetwork/web/servlets/LogoutServlet.java
@@ -1,6 +1,6 @@
 package com.getjavajob.training.timashovy.socialnetwork.web.servlets;
 
-import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -8,10 +8,23 @@ import java.io.IOException;
 
 public class LogoutServlet extends HttpServlet {
 
+    private static final int EXPIRATION_COOKIE_TIME = 0;
+
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         req.getSession().invalidate();
+        invalidateCookies(req, resp);
         resp.sendRedirect("/login");
+    }
+
+    private void invalidateCookies(HttpServletRequest req, HttpServletResponse resp) {
+        Cookie[] cookies = req.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                cookie.setMaxAge(EXPIRATION_COOKIE_TIME);
+                resp.addCookie(cookie);
+            }
+        }
     }
 
 }
