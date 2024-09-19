09c42225420eb7d0cae1fc3935f698c8bf03d3c4
yutimashov <timashov.yuriy@gmail.com>
2024-09-19 14:38:03 +0300

    delete account controller: rewrite using mvc pattern

--- web/src/main/java/com/getjavajob/training/timashovy/socialnetwork/web/util/StatusTypes.java
+++ web/src/main/java/com/getjavajob/training/timashovy/socialnetwork/web/util/StatusTypes.java
@@ -5,6 +5,7 @@ public final class StatusTypes {
     public static final String AUTH_DATA_ERROR = "?error=auth_data";
     public static final String REG_SUCCESS = "?reg=success";
     public static final String AUTHORIZATION_ERROR = "?error=authorization";
+    public static final String DELETE_ACCOUNT_SUCCESS = "?delete_account=success";
 
     /**
      * Class is not supposed to have any instance.
