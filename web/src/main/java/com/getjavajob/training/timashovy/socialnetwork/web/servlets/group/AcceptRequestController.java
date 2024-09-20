1c72a2435c763ec154124fa7408e0ef35cb9108b
yutimashov <timashov.yuriy@gmail.com>
2024-09-20 08:55:38 +0300

    accept group request servlet: rewrite using mvc pattern

---
+++ web/src/main/java/com/getjavajob/training/timashovy/socialnetwork/web/servlets/group/AcceptRequestController.java
@@ -0,0 +1,28 @@
+package com.getjavajob.training.timashovy.socialnetwork.web.servlets.group;
+
+import com.getjavajob.training.timashovy.socialnetwork.service.interfaces.GroupMembershipService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServlet;
+
+@Controller
+@RequestMapping("/group/accept-request")
+public class AcceptRequestController extends HttpServlet {
+
+    private final GroupMembershipService groupMembershipService;
+
+    public AcceptRequestController(GroupMembershipService groupMembershipService) {
+        this.groupMembershipService = groupMembershipService;
+    }
+
+    @GetMapping
+    protected String acceptRequest(@RequestParam("groupId") long groupId,
+                                   @RequestParam("accountId") long accountId) {
+        groupMembershipService.makeMember(groupId, accountId);
+        return "redirect:/group?id=" + groupId;
+    }
+
+}
