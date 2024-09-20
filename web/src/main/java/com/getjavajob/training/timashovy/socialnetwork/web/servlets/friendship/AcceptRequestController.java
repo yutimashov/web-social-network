302062a8e420fa65de14fd623945b1e4ec8bdb1a
yutimashov <timashov.yuriy@gmail.com>
2024-09-20 15:47:13 +0300

    accept friendship request servlet: rewrite using mvc pattern

---
+++ web/src/main/java/com/getjavajob/training/timashovy/socialnetwork/web/servlets/friendship/AcceptRequestController.java
@@ -0,0 +1,28 @@
+package com.getjavajob.training.timashovy.socialnetwork.web.servlets.friendship;
+
+import com.getjavajob.training.timashovy.socialnetwork.common.account.Account;
+import com.getjavajob.training.timashovy.socialnetwork.service.interfaces.AccountService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttribute;
+
+@Controller
+@RequestMapping("/friends/accept-request")
+public class AcceptRequestController {
+
+    private final AccountService accountService;
+
+    public AcceptRequestController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @GetMapping
+    protected String doGet(@SessionAttribute("account") Account account, @RequestParam("id") long requesterAccountId) {
+        Long accepterAccountId = account.getId();
+        accountService.addFriend(requesterAccountId, accepterAccountId);
+        return "redirect:/friends?id=" + accepterAccountId;
+    }
+
+}
