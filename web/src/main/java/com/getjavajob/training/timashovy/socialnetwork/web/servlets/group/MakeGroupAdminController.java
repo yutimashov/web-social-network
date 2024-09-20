0b21fdca4b6478e5863623bb443f06bb2f9ee96d
yutimashov <timashov.yuriy@gmail.com>
2024-09-20 14:01:14 +0300

    make group admin servlet: rewrite using mvc pattern

---
+++ web/src/main/java/com/getjavajob/training/timashovy/socialnetwork/web/servlets/group/MakeGroupAdminController.java
@@ -0,0 +1,27 @@
+package com.getjavajob.training.timashovy.socialnetwork.web.servlets.group;
+
+import com.getjavajob.training.timashovy.socialnetwork.service.interfaces.GroupMembershipService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServlet;
+
+@Controller
+@RequestMapping("/group/make-admin")
+public class MakeGroupAdminController extends HttpServlet {
+
+    private final GroupMembershipService groupMembershipService;
+
+    public MakeGroupAdminController(GroupMembershipService groupMembershipService) {
+        this.groupMembershipService = groupMembershipService;
+    }
+
+    @GetMapping
+    protected String doGet(@RequestParam("groupId") long groupId, @RequestParam("accountId") long accountId) {
+        groupMembershipService.makeAdmin(groupId, accountId);
+        return "redirect:/group?id=" + groupId;
+    }
+
+}
