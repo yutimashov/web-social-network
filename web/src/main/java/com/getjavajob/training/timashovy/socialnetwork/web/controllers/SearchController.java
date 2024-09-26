37edccd485c31226467a2f49eb437fe8f02dc5f4
yutimashov <timashov.yuriy@gmail.com>
2024-09-26 11:32:16 +0300

    fix search page results

--- web/src/main/java/com/getjavajob/training/timashovy/socialnetwork/web/controllers/SearchController.java
+++ web/src/main/java/com/getjavajob/training/timashovy/socialnetwork/web/controllers/SearchController.java
@@ -1,15 +1,11 @@
 package com.getjavajob.training.timashovy.socialnetwork.web.controllers;
 
-import com.getjavajob.training.timashovy.socialnetwork.common.Group;
-import com.getjavajob.training.timashovy.socialnetwork.common.account.Account;
 import com.getjavajob.training.timashovy.socialnetwork.service.interfaces.SearchService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.List;
 
 @RequestMapping("/search")
 @Controller
@@ -36,25 +32,22 @@ public class SearchController {
         } else if ("group".equals(searchType)) {
             numberOfPages = handleGroupSearch(searchQuery, currentPage, model);
         }
-        if (numberOfPages % RESULTS_PER_PAGE > 0) {
-            numberOfPages++;
-        }
         model.addAttribute("numberOfPages", numberOfPages);
         model.addAttribute("recordsPerPage", RESULTS_PER_PAGE);
         return "/search/result";
     }
 
     private int handleAccountSearch(String searchQuery, int currentPage, Model model) {
-        List<Account> accounts = searchService.findAccounts(searchQuery, currentPage, RESULTS_PER_PAGE);
-        model.addAttribute("accounts", accounts);
-        int totalResults = searchService.findAccountResultsAmount(searchQuery);
-        return (int) Math.ceil((double) totalResults / RESULTS_PER_PAGE);
+        model.addAttribute("accounts", searchService.findAccounts(searchQuery, currentPage, RESULTS_PER_PAGE));
+        return calculateNumberOfPages(searchService.findAccountResultsAmount(searchQuery));
     }
 
     private int handleGroupSearch(String searchQuery, int currentPage, Model model) {
-        List<Group> groups = searchService.findGroups(searchQuery, currentPage, RESULTS_PER_PAGE);
-        model.addAttribute("groups", groups);
-        int totalResults = searchService.findGroupResultsAmount(searchQuery);
+        model.addAttribute("groups", searchService.findGroups(searchQuery, currentPage, RESULTS_PER_PAGE));
+        return calculateNumberOfPages(searchService.findGroupResultsAmount(searchQuery));
+    }
+
+    private int calculateNumberOfPages(int totalResults) {
         return (int) Math.ceil((double) totalResults / RESULTS_PER_PAGE);
     }
 
