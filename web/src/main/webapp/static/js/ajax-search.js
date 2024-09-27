435c08c015ce83367bd7d88ee4e5fe4d0963904c
yutimashov <timashov.yuriy@gmail.com>
2024-09-27 14:55:27 +0300

    fix dynaminc search: refactor script

--- web/src/main/webapp/static/js/ajax-search.js
+++ web/src/main/webapp/static/js/ajax-search.js
@@ -1,26 +1,31 @@
 const dropdownContainer = document.getElementById('dropdown-container');
-let searchResultPortion = 1;
+let searchPortion = 1;
 let allResultsLoaded = false;
+let itemsPerRequest = 0;
 
 document.getElementById('searchQuery').addEventListener('input', loadSearchTips);
 
 function loadSearchTips() {
     const searchQuery = document.getElementById('searchQuery').value;
     const searchType = document.getElementById('searchType').value;
+
     if (searchQuery.length > 0) {
         const xhr = new XMLHttpRequest();
-        xhr.open("GET", "/search_ajax?searchQuery=" + encodeURIComponent(searchQuery)
-            + "&searchType=" + encodeURIComponent(searchType) + "&currentPage=" + searchResultPortion, true);
+        xhr.open("GET",
+            `/search_ajax?searchQuery=${encodeURIComponent(searchQuery)}&searchType=${encodeURIComponent(searchType)}&currentPage=${searchPortion}`,
+            true);
         xhr.timeout = 6000;
         xhr.onload = function () {
             if (xhr.status === 200) {
+                allResultsLoaded = false;
                 const searchResult = xhr.responseText;
                 if (searchResult.trim() === '') {
                     dropdownContainer.classList.remove('show');
                 } else {
-                    searchResultPortion++;
+                    searchPortion++;
                     dropdownContainer.classList.add('show');
                     dropdownContainer.innerHTML = searchResult;
+                    itemsPerRequest = document.querySelectorAll('.dropdown-item').length;
                 }
             } else {
                 console.log("ERROR : ", xhr.statusText);
@@ -41,49 +46,44 @@ document.addEventListener('click', function (e) {
     const isClickInside = dropdownContainer.contains(e.target);
     if (!isClickInside) {
         dropdownContainer.classList.remove('show');
-        searchResultPortion = 1;
+        searchPortion = 1;
     }
 });
 
 // dynamic loading search results
 dropdownContainer.addEventListener('scroll', () => {
-    appendSearchResultsDynamically();
+    if (!allResultsLoaded && dropdownContainer.clientHeight + dropdownContainer.scrollTop >= dropdownContainer.scrollHeight) {
+        appendSearchResultsDynamically();
+    }
 })
 
 function appendSearchResultsDynamically() {
-    if (allResultsLoaded) {
-        return;
-    }
-    if (dropdownContainer.clientHeight + dropdownContainer.scrollTop > dropdownContainer.scrollHeight) {
-        const clientSearchQuery = document.getElementById('searchQuery').value;
-        const searchType = document.getElementById('searchType').value;
-        const xhr = new XMLHttpRequest();
-        xhr.open("GET", "/search_ajax?searchQuery=" + encodeURIComponent(clientSearchQuery)
-            + "&searchType=" + encodeURIComponent(searchType) + "&currentPage="
-            + encodeURIComponent(searchResultPortion), true);
-        xhr.timeout = 6000;
-        xhr.onload = function () {
-            if (xhr.status === 200) {
-                if (xhr.responseText.trim() === "") {
-                    allResultsLoaded = true;
-                } else {
-                    searchResultPortion++;
-                    dropdownContainer.insertAdjacentHTML("beforeend", xhr.responseText);
-                    // if latest query returned less than 5 elements - it was the last query
-                    if (document.querySelectorAll('.dropdown-item') % 5 !== 0) {
-                        allResultsLoaded = true;
-                    }
-                }
-            } else {
-                console.log("ERROR : ", xhr.statusText);
+    const searchQuery = document.getElementById('searchQuery').value;
+    const searchType = document.getElementById('searchType').value;
+
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET",
+        `/search_ajax?searchQuery=${encodeURIComponent(searchQuery)}&searchType=${encodeURIComponent(searchType)}&currentPage=${searchPortion}`,
+        true);
+    xhr.timeout = 6000;
+    xhr.onload = function () {
+        if (xhr.status === 200) {
+            const searchResult = xhr.responseText;
+            dropdownContainer.insertAdjacentHTML("beforeend", searchResult);
+            searchPortion++;
+            // if latest query returned less than elements contained in 1 portion - it was the last query
+            if (document.querySelectorAll('.dropdown-item').length % itemsPerRequest !== 0) {
+                allResultsLoaded = true;
             }
-        };
-        xhr.ontimeout = function () {
-            console.log("ERROR: Request timed out");
-        };
-        xhr.onerror = function () {
-            console.log("ERROR: Network error");
-        };
-        xhr.send();
-    }
+        } else {
+            console.log("ERROR : ", xhr.statusText);
+        }
+    };
+    xhr.ontimeout = function () {
+        console.log("ERROR: Request timed out");
+    };
+    xhr.onerror = function () {
+        console.log("ERROR: Network error");
+    };
+    xhr.send();
 }
