572c5c4b4b3734ef5e4919d5cb18e199c82c18ce
yutimashov <timashov.yuriy@gmail.com>
2024-10-28 16:24:09 +0300

    make friendship class as entity

--- domain/src/main/java/com/getjavajob/training/timashovy/socialnetwork/domain/friendship/Friendship.java
+++ domain/src/main/java/com/getjavajob/training/timashovy/socialnetwork/domain/friendship/Friendship.java
@@ -2,9 +2,9 @@ package com.getjavajob.training.timashovy.socialnetwork.domain.friendship;
 
 import com.getjavajob.training.timashovy.socialnetwork.domain.account.Account;
 
+import javax.persistence.*;
+import java.io.Serializable;
 import java.util.Objects;
-
-import static java.util.Objects.hash;
 
 /**
  * Model of Friendship entity in application.
@@ -17,11 +17,24 @@ import static java.util.Objects.hash;
  * @author Yuriy Timashov
  * @since 13.01.2024
  */
+@Entity
+@IdClass(Friendship.FriendshipId.class)
 public class Friendship {
 
+    @Id
+    @ManyToOne
+    @JoinColumn(name = "requester_id")
     private Account requester;
+
+    @Id
+    @ManyToOne
+    @JoinColumn(name = "accepter_id")
     private Account receiver;
+
     private boolean friendshipStatus;
+
+    public Friendship() {
+    }
 
     public Friendship(Account requester, Account receiver, boolean friendshipStatus) {
         this.requester = requester;
@@ -55,26 +68,66 @@ public class Friendship {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (!(o instanceof Friendship)) {
-            return false;
-        }
-        Friendship friendship = (Friendship) o;
-        return Objects.equals(requester, friendship.requester) && Objects.equals(receiver, friendship.receiver)
-                && Objects.equals(friendshipStatus, friendship.friendshipStatus);
+        if (this == o) return true;
+        if (!(o instanceof Friendship)) return false;
+        Friendship that = (Friendship) o;
+        return Objects.equals(requester, that.requester) &&
+                Objects.equals(receiver, that.receiver);
     }
 
     @Override
     public int hashCode() {
-        return hash(requester, receiver, friendshipStatus);
+        return Objects.hash(requester, receiver);
     }
 
     @Override
     public String toString() {
         return "Friendship{ requester=" + requester + ", receiver=" + receiver + ", friendshipStatus="
                 + friendshipStatus + "}";
+    }
+
+    public static class FriendshipId implements Serializable {
+
+        private Long requesterId;
+        private Long receiverId;
+
+        public FriendshipId() {
+        }
+
+        public FriendshipId(Long requesterId, Long receiverId) {
+            this.requesterId = requesterId;
+            this.receiverId = receiverId;
+        }
+
+        public Long getRequesterId() {
+            return requesterId;
+        }
+
+        public void setRequesterId(Long requesterId) {
+            this.requesterId = requesterId;
+        }
+
+        public Long getReceiverId() {
+            return receiverId;
+        }
+
+        public void setReceiverId(Long receiverId) {
+            this.receiverId = receiverId;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof FriendshipId)) return false;
+            FriendshipId that = (FriendshipId) o;
+            return Objects.equals(requesterId, that.requesterId) &&
+                    Objects.equals(receiverId, that.receiverId);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(requesterId, receiverId);
+        }
     }
 
 }
