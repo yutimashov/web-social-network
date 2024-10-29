d08c861b103bfde4adbff7fd5b98b5dfb35e6132
yutimashov <timashov.yuriy@gmail.com>
2024-10-29 22:39:31 +0300

    start implementing Account dao class

--- dao/src/main/java/com/getjavajob/training/timashovy/socialnetwork/dao/daoimpl/account/AccountDaoImpl.java
+++ dao/src/main/java/com/getjavajob/training/timashovy/socialnetwork/dao/daoimpl/account/AccountDaoImpl.java
@@ -4,24 +4,18 @@ import com.getjavajob.training.timashovy.socialnetwork.dao.interfaces.BaseDao;
 import com.getjavajob.training.timashovy.socialnetwork.dao.interfaces.account.PhoneDao;
 import com.getjavajob.training.timashovy.socialnetwork.domain.account.Account;
 import com.getjavajob.training.timashovy.socialnetwork.domain.phone.Phone;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.support.GeneratedKeyHolder;
-import org.springframework.jdbc.support.KeyHolder;
 
-import javax.sql.DataSource;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.List;
 import java.util.Optional;
 
 import static com.getjavajob.training.timashovy.socialnetwork.dao.util.dbutils.TableNames.ACCOUNTS_TABLE;
 import static com.getjavajob.training.timashovy.socialnetwork.dao.util.dbutils.fieldsnames.AccountTableFields.*;
-import static com.getjavajob.training.timashovy.socialnetwork.domain.account.AccountRole.REGULAR;
 import static com.getjavajob.training.timashovy.socialnetwork.domain.account.AccountRole.valueOf;
 import static com.getjavajob.training.timashovy.socialnetwork.domain.phone.PhoneType.PERSONAL;
 import static com.getjavajob.training.timashovy.socialnetwork.domain.phone.PhoneType.WORKING;
-import static java.lang.String.valueOf;
 import static java.util.Objects.isNull;
 import static java.util.Optional.empty;
 import static java.util.Optional.of;
@@ -33,11 +27,9 @@ import static java.util.stream.Collectors.toList;
  */
 public class AccountDaoImpl implements BaseDao<Account> {
 
-    private static final String CREATE = "INSERT INTO " + ACCOUNTS_TABLE + " (" + ACCOUNT_FIRST_NAME + ", "
-            + ACCOUNT_LAST_NAME + ", " + ACCOUNT_MIDDLE_NAME + ", " + ACCOUNT_BIRTH_DATE + ", "
-            + ACCOUNT_PERSONAL_ADDRESS + ", " + ACCOUNT_WORK_ADDRESS + ", " + ACCOUNT_EMAIL + ", " + ACCOUNT_ICQ + ", "
-            + ACCOUNT_SKYPE + ", " + ACCOUNT_ADDITIONAL_INFO + ", " + ACCOUNT_ROLE_TYPE + ", " + ACCOUNT_AVATAR
-            + ") VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);";
+    @PersistenceContext
+    protected EntityManager entityManager;
+
     private static final String GET_BY_ID = "SELECT " + ACCOUNT_ID + ", " + ACCOUNT_FIRST_NAME + ", "
             + ACCOUNT_LAST_NAME + ", " + ACCOUNT_MIDDLE_NAME + ", " + ACCOUNT_BIRTH_DATE + ", "
             + ACCOUNT_PERSONAL_ADDRESS + ", " + ACCOUNT_WORK_ADDRESS + ", " + ACCOUNT_EMAIL + ", " + ACCOUNT_ICQ + ", "
@@ -54,7 +46,6 @@ public class AccountDaoImpl implements BaseDao<Account> {
             + ACCOUNT_ICQ + "= ?, " + ACCOUNT_SKYPE + "= ?, " + ACCOUNT_ADDITIONAL_INFO + " = ?, " + ACCOUNT_ROLE_TYPE
             + " = ?, " + ACCOUNT_AVATAR + " = ? WHERE " + ACCOUNT_ID + " = ?;";
     private static final String DELETE_BY_ID = "DELETE FROM " + ACCOUNTS_TABLE + " WHERE " + ACCOUNT_ID + " = ?;";
-    private JdbcTemplate jdbcTemplate;
     private final PhoneDao phoneDao;
     private final RowMapper<Account> accountRowMapper = (rs, rowNum) -> new Account.Builder()
             .id(rs.getLong(ACCOUNT_ID))
@@ -76,43 +67,11 @@ public class AccountDaoImpl implements BaseDao<Account> {
         this.phoneDao = phoneDao;
     }
 
-    /**
-     * @param dataSource specific DataSource, defining database to work with
-     */
-    public void setDataSource(DataSource dataSource) {
-        this.jdbcTemplate = new JdbcTemplate(dataSource);
-    }
-
     @Override
     public Long create(Account account) {
-        KeyHolder keyHolder = new GeneratedKeyHolder();
-        jdbcTemplate.update(conn -> {
-            PreparedStatement ps = conn.prepareStatement(CREATE, new String[]{ACCOUNT_ID});
-            setAccountData(account, ps);
-            return ps;
-        }, keyHolder);
-        Number generatedId = keyHolder.getKey();
-        if (generatedId != null) {
-            Long id = generatedId.longValue();
-            account.setId(id);
-            return id;
-        }
-        return null;
-    }
-
-    private void setAccountData(Account account, PreparedStatement ps) throws SQLException {
-        ps.setString(1, account.getFirstName());
-        ps.setString(2, account.getLastName());
-        ps.setString(3, account.getMiddleName());
-        ps.setObject(4, account.getBirthDate());
-        ps.setString(5, account.getPersonalAddress());
-        ps.setString(6, account.getWorkAddress());
-        ps.setString(7, account.getEmail());
-        ps.setString(8, account.getIcq());
-        ps.setString(9, account.getSkype());
-        ps.setString(10, account.getAdditionalInfo());
-        ps.setString(11, isNull(account.getRole()) ? valueOf(REGULAR) : account.getRole().name());
-        ps.setBinaryStream(12, account.getAvatar());
+        entityManager.persist(account);
+        entityManager.flush();
+        return account.getId();
     }
 
     @Override
@@ -125,7 +84,23 @@ public class AccountDaoImpl implements BaseDao<Account> {
 
     @Override
     public boolean deleteById(Long id) {
-        return jdbcTemplate.update(DELETE_BY_ID, id) > 0;
+        try {
+            entityManager.getTransaction().begin();
+            Account account = entityManager.find(Account.class, id);
+            if (account != null) {
+                entityManager.remove(account);
+                entityManager.getTransaction().commit();
+                return true;
+            } else {
+                entityManager.getTransaction().rollback();
+                return false;
+            }
+        } catch (IllegalArgumentException e) {
+            if (entityManager.getTransaction().isActive()) {
+                entityManager.getTransaction().rollback();
+            }
+            return false;
+        }
     }
 
     @Override
