61810ab8928bdee07b5a3eea9a4d5d7870530d47
yutimashov <timashov.yuriy@gmail.com>
2024-10-31 13:45:58 +0300

    rewrite SearchAccountDaoImpl class using jpa

--- dao/src/main/java/com/getjavajob/training/timashovy/socialnetwork/dao/daoimpl/search/SearchAccountDaoImpl.java
+++ dao/src/main/java/com/getjavajob/training/timashovy/socialnetwork/dao/daoimpl/search/SearchAccountDaoImpl.java
@@ -1,14 +1,14 @@
 package com.getjavajob.training.timashovy.socialnetwork.dao.daoimpl.search;
 
-import com.getjavajob.training.timashovy.socialnetwork.domain.account.Account;
 import com.getjavajob.training.timashovy.socialnetwork.dao.interfaces.SearchDao;
-import org.springframework.jdbc.core.JdbcTemplate;
+import com.getjavajob.training.timashovy.socialnetwork.domain.account.Account;
 import org.springframework.jdbc.core.RowMapper;
 
-import javax.sql.DataSource;
+import javax.persistence.EntityManager;
+import javax.persistence.NonUniqueResultException;
+import javax.persistence.PersistenceContext;
 import java.util.List;
 
-import static com.getjavajob.training.timashovy.socialnetwork.dao.util.dbutils.TableNames.ACCOUNTS_TABLE;
 import static com.getjavajob.training.timashovy.socialnetwork.dao.util.dbutils.fieldsnames.AccountTableFields.*;
 
 /**
@@ -17,32 +17,33 @@ import static com.getjavajob.training.timashovy.socialnetwork.dao.util.dbutils.f
  */
 public class SearchAccountDaoImpl implements SearchDao<Account> {
 
-    private static final String FIND_ACCOUNTS = "SELECT " + ACCOUNT_ID + ", " + ACCOUNT_FIRST_NAME + ", "
-            + ACCOUNT_LAST_NAME + " FROM " + ACCOUNTS_TABLE + " WHERE " + ACCOUNT_FIRST_NAME + " ILIKE ? OR "
-            + ACCOUNT_LAST_NAME + " ILIKE ? OFFSET ? LIMIT ?;";
-    private static final String FIND_ACCOUNTS_AMOUNT = "SELECT COUNT(*) AS " + TOTAL_ACCOUNTS_AMOUNT_ALIAS + " FROM "
-            + ACCOUNTS_TABLE + " WHERE " + ACCOUNT_FIRST_NAME + " ILIKE ? OR " + ACCOUNT_LAST_NAME + " ILIKE ?;";
-    private JdbcTemplate jdbcTemplate;
+    @PersistenceContext
+    private EntityManager entityManager;
+
     private final RowMapper<Account> searchAccountRowMapper = (rs, rowNumber) -> new Account.Builder()
             .id(rs.getLong(ACCOUNT_ID))
             .firstName(rs.getString(ACCOUNT_FIRST_NAME))
             .lastName(rs.getString(ACCOUNT_LAST_NAME))
             .build();
 
-    public void setDataSource(DataSource dataSource) {
-        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    @Override
+    public List<Account> searchAccounts(String searchQuery, int currentPage, int recordsPerPage) {
+        return entityManager.createQuery("SELECT a FROM Account a WHERE LOWER(a.firstName) "
+                        + "LIKE LOWER(:searchQuery) OR LOWER(a.lastName) LIKE LOWER(:searchQuery)", Account.class)
+                .setParameter("searchQuery", "%" + searchQuery + "%")
+                .setFirstResult(currentPage * recordsPerPage - recordsPerPage)
+                .setMaxResults(recordsPerPage)
+                .getResultList();
     }
 
     @Override
-    public List<Account> searchAccounts(String searchQuery, int currentPage, int recordsPerPage) {
-        return jdbcTemplate.query(FIND_ACCOUNTS, searchAccountRowMapper, "%" + searchQuery + "%",
-                "%" + searchQuery + "%", currentPage * recordsPerPage - recordsPerPage, recordsPerPage);
-    }
-
     public int findResultsAmount(String searchQuery) {
-        Integer total = jdbcTemplate.queryForObject(FIND_ACCOUNTS_AMOUNT, Integer.class, "%" + searchQuery + "%",
-                "%" + searchQuery + "%");
-        return (total != null) ? total : -1;
+        try {
+            return entityManager.createQuery("select count(*) from Account a where a.firstName "
+                    + "ilike :searchQuery or a.lastName ilike :sesrchQuery", Integer.class).getSingleResult();
+        } catch (NonUniqueResultException e) {
+            return -1;
+        }
     }
 
 }
