5e353cc730f5ef61bb6778095eb4ef8e371b67d9
yutimashov <timashov.yuriy@gmail.com>
2024-11-05 10:45:40 +0300

    refactor AuthController class

--- web/src/main/java/com/getjavajob/training/timashovy/socialnetwork/web/controllers/AuthController.java
+++ web/src/main/java/com/getjavajob/training/timashovy/socialnetwork/web/controllers/AuthController.java
@@ -7,10 +7,13 @@ import com.getjavajob.training.timashovy.socialnetwork.service.interfaces.Passwo
 import com.getjavajob.training.timashovy.socialnetwork.web.dto.AccountDto;
 import com.getjavajob.training.timashovy.socialnetwork.web.mappers.AccountMapper;
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
@@ -18,21 +21,22 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.util.Optional;
 
-import static com.getjavajob.training.timashovy.socialnetwork.web.util.StatusTypes.AUTH_DATA_ERROR;
-import static com.getjavajob.training.timashovy.socialnetwork.web.util.StatusTypes.REG_SUCCESS;
-import static java.util.concurrent.TimeUnit.HOURS;
+import static com.getjavajob.training.timashovy.socialnetwork.web.util.UrlStatusParameter.AUTH_DATA_ERROR;
+import static com.getjavajob.training.timashovy.socialnetwork.web.util.UrlStatusParameter.REG_SUCCESS;
+import static java.util.Objects.isNull;
+import static org.slf4j.LoggerFactory.getLogger;
 
 @SessionAttributes("account")
 @Controller
 public class AuthController {
 
     private static final int EXPIRATION_COOKIE_LIFE_TIME = 0;
-    private final static int REMEMBER_ME_COOKIE_LIFE_TIME = (int) HOURS.toSeconds(1);
+    private static final int REMEMBER_ME_COOKIE_LIFE_TIME = 3600;
     private final LoginService loginService;
     private final PasswordService passwordService;
     private final AccountService accountService;
 
-    private final static Logger logger = LoggerFactory.getLogger(AccountController.class);
+    private final static Logger logger = getLogger(AccountController.class);
 
     public AuthController(LoginService loginService, PasswordService passwordService, AccountService accountService) {
         this.loginService = loginService;
@@ -48,33 +52,34 @@ public class AuthController {
     @PostMapping("/login")
     public String login(@RequestParam String email,
                         @RequestParam String password,
-                        @RequestParam Optional<String> rememberMe,
+                        @RequestParam(required = false) String rememberMe,
                         Model model,
                         HttpServletResponse resp) {
         Optional<Account> loggedInAccount = loginService.getLoggedInAccount(email, password);
         if (loggedInAccount.isPresent()) {
             Account account = loggedInAccount.get();
             model.addAttribute("account", account);
-            if (rememberMe.isPresent()) {
-                createRememberMeCookies(account, resp);
+            if (!isNull(rememberMe)) {
+                createRememberMeCookie(account, resp);
             }
             return "redirect:/account?id=" + account.getId();
         } else {
-            return "redirect:/login" + AUTH_DATA_ERROR;
+            return "redirect:/login" + AUTH_DATA_ERROR.getValue();
         }
     }
 
-    private void createRememberMeCookies(Account account, HttpServletResponse resp) {
-        prepareCookie(resp, "login", account.getEmail());
+    private void createRememberMeCookie(Account account, HttpServletResponse resp) {
+        resp.addCookie(createCookie("login", account.getEmail()));
         if (passwordService.get(account.getId()).isPresent()) {
-            prepareCookie(resp, "password", passwordService.get(account.getId()).get().getPasswordValue());
+            resp.addCookie(createCookie("password", passwordService.get(account.getId()).get()
+                    .getPasswordValue()));
         }
     }
 
-    private void prepareCookie(HttpServletResponse resp, String cookieName, String cookieValue) {
+    private Cookie createCookie(String cookieName, String cookieValue) {
         Cookie cookie = new Cookie(cookieName, cookieValue);
         cookie.setMaxAge(REMEMBER_ME_COOKIE_LIFE_TIME);
-        resp.addCookie(cookie);
+        return cookie;
     }
 
     @GetMapping("/register")
@@ -88,26 +93,30 @@ public class AuthController {
                                              @RequestParam("personalPhones") String personalPhones,
                                              @RequestParam("workingPhones") String workingPhones) {
         accountService.create(new AccountMapper().toAccount(accountDto), password, personalPhones, workingPhones);
-        return "redirect:/login" + REG_SUCCESS;
+        return "redirect:/login" + REG_SUCCESS.getValue();
     }
 
     @GetMapping("/logout")
     public String logoutPage(HttpSession session, HttpServletRequest req, HttpServletResponse resp) {
         session.invalidate();
-        clearCookies(req, resp);
+        clearAllCookies(req, resp);
         return "redirect:/login";
     }
 
-    private void clearCookies(HttpServletRequest req, HttpServletResponse resp) {
+    private void clearAllCookies(HttpServletRequest req, HttpServletResponse resp) {
         Cookie[] cookies = req.getCookies();
         if (cookies != null) {
             for (Cookie cookie : cookies) {
-                cookie.setMaxAge(EXPIRATION_COOKIE_LIFE_TIME);
-                cookie.setValue(null);
-                cookie.setPath("/");
-                resp.addCookie(cookie);
+                resp.addCookie(clearCookie(cookie));
             }
         }
+    }
+
+    private Cookie clearCookie(Cookie cookie) {
+        cookie.setMaxAge(EXPIRATION_COOKIE_LIFE_TIME);
+        cookie.setValue(null);
+        cookie.setPath("/");
+        return cookie;
     }
 
 }
---
+++ web/src/main/java/com/getjavajob/training/timashovy/socialnetwork/web/util/UrlStatusParameter.java
@@ -0,0 +1,20 @@
+package com.getjavajob.training.timashovy.socialnetwork.web.util;
+
+public enum UrlStatusParameter {
+
+    AUTH_DATA_ERROR("?error=auth_data"),
+    REG_SUCCESS("?reg=success"),
+    AUTHORIZATION_ERROR("?error=authorization"),
+    DELETE_ACCOUNT_SUCCESS("?delete_account=success");
+
+    private final String value;
+
+    UrlStatusParameter(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+}
