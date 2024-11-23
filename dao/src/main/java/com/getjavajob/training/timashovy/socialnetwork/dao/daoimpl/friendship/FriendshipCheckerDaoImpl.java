21eabda650fb66bdfc2c1d7c42098ca5a0f8421b
yutimashov <timashov.yuriy@gmail.com>
2024-11-23 18:27:03 +0300

    add unit tests on FriendshipCheckerDaoImpl class

--- dao/src/main/java/com/getjavajob/training/timashovy/socialnetwork/dao/daoimpl/friendship/FriendshipCheckerDaoImpl.java
+++ dao/src/main/java/com/getjavajob/training/timashovy/socialnetwork/dao/daoimpl/friendship/FriendshipCheckerDaoImpl.java
@@ -1,9 +1,14 @@
 package com.getjavajob.training.timashovy.socialnetwork.dao.daoimpl.friendship;
 
+import com.getjavajob.training.timashovy.socialnetwork.dao.exception.DaoException;
 import com.getjavajob.training.timashovy.socialnetwork.dao.interfaces.friendship.FriendshipCheckerDao;
+import org.slf4j.Logger;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceException;
+
+import static org.slf4j.LoggerFactory.getLogger;
 
 /**
  * Singleton class responsible for working with `account_data.friendship` table in DB.
@@ -11,19 +16,27 @@ import javax.persistence.PersistenceContext;
  */
 public class FriendshipCheckerDaoImpl implements FriendshipCheckerDao {
 
+    private static final Logger logger = getLogger(FriendshipCheckerDaoImpl.class);
+
     @PersistenceContext
     private EntityManager entityManager;
 
     @Override
     public boolean checkFriendshipRecordExistence(Long requesterId, Long accepterId) {
-        return entityManager.createQuery(
-                        "select 1 from Friendship f where f.initiatorAccountId = :requesterId "
-                                + "and f.friendAccountId = :accepterId"
-                )
-                .setParameter("requesterId", getFirstId(requesterId, accepterId))
-                .setParameter("accepterId", getSecondId(requesterId, accepterId))
-                .getResultList()
-                .size() > 0;
+        try {
+            return entityManager.createQuery(
+                            "select 1 from Friendship f where f.initiatorAccountId = :requesterId "
+                                    + "and f.friendAccountId = :accepterId"
+                    )
+                    .setParameter("requesterId", getFirstId(requesterId, accepterId))
+                    .setParameter("accepterId", getSecondId(requesterId, accepterId))
+                    .getResultList()
+                    .size() > 0;
+        } catch (PersistenceException e) {
+            logger.error("Error checking if friendship record already exists: requesterId={}, accepterId={}",
+                    requesterId, accepterId);
+            throw new DaoException("Cannot check if friendship record already exists", e);
+        }
     }
 
     private Long getFirstId(Long requesterId, Long accepterId) {
