0be989a62f4b5618f9fc1ef9c37654a8c02677e9
yutimashov <timashov.yuriy@gmail.com>
2024-11-24 11:00:48 +0300

    add overrided annotations

--- domain/src/main/java/com/getjavajob/training/timashovy/socialnetwork/domain/account/Account.java
+++ domain/src/main/java/com/getjavajob/training/timashovy/socialnetwork/domain/account/Account.java
@@ -215,10 +215,12 @@ public class Account implements BaseEntity<Long> {
         phone.setAccount(null);
     }
 
+    @Override
     public Long getId() {
         return id;
     }
 
+    @Override
     public void setId(Long id) {
         this.id = id;
     }
