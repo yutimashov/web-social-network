204f1df0c16e0f4a39cdee4c87b7c6a480588d6c
yutimashov <timashov.yuriy@gmail.com>
2024-12-13 11:29:41 +0300

    add config for dao layer

---
+++ service/src/main/java/com/getjavajob/training/timashovy/socialnetwork/service/PersistenceConfig.java
@@ -0,0 +1,74 @@
+package com.getjavajob.training.timashovy.socialnetwork.service;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jndi.JndiObjectFactoryBean;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories(basePackages = "com.getjavajob.training.timashovy.socialnetwork.dao.interfaces.group")
+public class PersistenceConfig {
+
+//    @Bean
+//    public DataSource dataSource() {
+//        JndiObjectFactoryBean jndiObjectFactoryBean = new JndiObjectFactoryBean();
+//        jndiObjectFactoryBean.setJndiName("java:/comp/env/jdbc/socialNetwork");
+//        jndiObjectFactoryBean.setResourceRef(true);
+//        jndiObjectFactoryBean.setProxyInterface(DataSource.class);
+//        return (DataSource) jndiObjectFactoryBean.getObject();
+//    }
+
+    @Bean
+    public DataSource dataSource() {
+        JndiObjectFactoryBean jndiObjectFactoryBean = new JndiObjectFactoryBean();
+        jndiObjectFactoryBean.setJndiName("java:/comp/env/jdbc/socialNetwork");
+        jndiObjectFactoryBean.setResourceRef(true);
+        jndiObjectFactoryBean.setProxyInterface(DataSource.class);
+        try {
+            // Необходимо для правильной инициализации
+            jndiObjectFactoryBean.afterPropertiesSet();
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to initialize JNDI DataSource", e);
+        }
+        Object object = jndiObjectFactoryBean.getObject();
+        if (object instanceof DataSource) {
+            return (DataSource) object;
+        } else {
+            throw new RuntimeException("JNDI resource is not a DataSource");
+        }
+    }
+
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
+        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(dataSource);
+        em.setPackagesToScan("com.getjavajob.training.timashovy.socialnetwork.domain");
+
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        vendorAdapter.setShowSql(true);
+        vendorAdapter.setGenerateDdl(false);
+        vendorAdapter.setDatabasePlatform("org.hibernate.dialect.PostgreSQL95Dialect");
+
+        em.setJpaVendorAdapter(vendorAdapter);
+        // Properties jpaProperties = new Properties();
+        // jpaProperties.put("hibernate.format_sql", true);
+        // jpaProperties.put("hibernate.use_sql_comments", true);
+        // em.setJpaProperties(jpaProperties);
+        return em;
+    }
+
+    @Bean
+    public JpaTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+        return new JpaTransactionManager(entityManagerFactory);
+    }
+
+}
