f3e17c3f7308872ae07085ae7182baca24c7b3fc
yutimashov <timashov.yuriy@gmail.com>
2024-12-17 16:37:17 +0300

    add dummy form authentication

--- web/src/main/java/com/getjavajob/training/timashovy/socialnetwork/web/security/config/WebSecurityConfig.java
+++ web/src/main/java/com/getjavajob/training/timashovy/socialnetwork/web/security/config/WebSecurityConfig.java
@@ -1,23 +1,20 @@
 package com.getjavajob.training.timashovy.socialnetwork.web.security.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http
-                .httpBasic().and()
-                .authorizeHttpRequests()
-                .anyRequest().authenticated()
-                .and().build();
-    }
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        return http
+//                .httpBasic().and()
+//                .authorizeHttpRequests()
+//                .anyRequest().authenticated()
+//                .and().build();
+//    }
 
 //    @Bean
 //    public UserDetailsService userDetailsService() {
