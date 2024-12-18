5aeee29fdede6e893455334a48216749cdd11282
yutimashov <timashov.yuriy@gmail.com>
2024-12-18 16:36:37 +0300

    fix: refactor code (login) using security framework instead of manual implementation

--- service/src/main/java/com/getjavajob/training/timashovy/socialnetwork/service/serviceimpl/account/LoginServiceImpl.java
+++ service/src/main/java/com/getjavajob/training/timashovy/socialnetwork/service/serviceimpl/account/LoginServiceImpl.java
@@ -1,46 +1,31 @@
 package com.getjavajob.training.timashovy.socialnetwork.service.serviceimpl.account;
 
-import com.getjavajob.training.timashovy.socialnetwork.domain.account.Account;
-import com.getjavajob.training.timashovy.socialnetwork.domain.password.Password;
-import com.getjavajob.training.timashovy.socialnetwork.service.interfaces.AccountService;
-import com.getjavajob.training.timashovy.socialnetwork.service.interfaces.LoginService;
-import com.getjavajob.training.timashovy.socialnetwork.service.interfaces.PasswordService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Service;
-
-import java.util.Optional;
-
-import static com.getjavajob.training.timashovy.socialnetwork.service.util.PasswordUtil.hashCredentialData;
-import static java.util.Objects.isNull;
-import static java.util.Optional.empty;
-
-@Service
-public class LoginServiceImpl implements LoginService {
-
-    private final AccountService accountService;
-    private final PasswordService passwordService;
-
-    private final static Logger logger = LoggerFactory.getLogger(LoginServiceImpl.class);
-
-    public LoginServiceImpl(AccountService accountService, PasswordService passwordService) {
-        this.accountService = accountService;
-        this.passwordService = passwordService;
-    }
-
-    @Override
-    public Optional<Account> getLoggedInAccount(String email, String password) {
-        if (isNull(email) || isNull(password)) {
-            return empty();
-        }
-        Optional<Password> accountPassword = passwordService.findPasswordByEmail(email);
-        if (!accountPassword.isPresent()) {
-            return empty();
-        }
-        Password dbPassword = accountPassword.get();
-        return dbPassword.getPasswordValue().equals(hashCredentialData(password, dbPassword.getSalt()))
-                ? accountService.getById(dbPassword.getId())
-                : empty();
-    }
-
-}
+//@Service
+//public class LoginServiceImpl implements LoginService {
+//
+//    private final AccountService accountService;
+//    private final PasswordService passwordService;
+//
+//    private final static Logger logger = LoggerFactory.getLogger(LoginServiceImpl.class);
+//
+//    public LoginServiceImpl(AccountService accountService, PasswordService passwordService) {
+//        this.accountService = accountService;
+//        this.passwordService = passwordService;
+//    }
+//
+//    @Override
+//    public Optional<Account> getLoggedInAccount(String email, String password) {
+//        if (isNull(email) || isNull(password)) {
+//            return empty();
+//        }
+//        Optional<Password> accountPassword = passwordService.findPasswordByEmail(email);
+//        if (!accountPassword.isPresent()) {
+//            return empty();
+//        }
+//        Password dbPassword = accountPassword.get();
+//        return dbPassword.getPasswordValue().equals(hashCredentialData(password, dbPassword.getSalt()))
+//                ? accountService.getById(dbPassword.getId())
+//                : empty();
+//    }
+//
+//}
--- web/src/main/java/com/getjavajob/training/timashovy/socialnetwork/web/controllers/AuthController.java
+++ web/src/main/java/com/getjavajob/training/timashovy/socialnetwork/web/controllers/AuthController.java
@@ -2,24 +2,23 @@ package com.getjavajob.training.timashovy.socialnetwork.web.controllers;
 
 import com.getjavajob.training.timashovy.socialnetwork.domain.account.Account;
 import com.getjavajob.training.timashovy.socialnetwork.service.interfaces.AccountService;
-import com.getjavajob.training.timashovy.socialnetwork.service.interfaces.LoginService;
 import com.getjavajob.training.timashovy.socialnetwork.service.interfaces.PasswordService;
 import com.getjavajob.training.timashovy.socialnetwork.web.dto.AccountDto;
 import com.getjavajob.training.timashovy.socialnetwork.web.mappers.AccountMapper;
 import org.slf4j.Logger;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import java.util.Optional;
 
-import static com.getjavajob.training.timashovy.socialnetwork.web.util.UrlStatusParameter.AUTH_DATA_ERROR;
 import static com.getjavajob.training.timashovy.socialnetwork.web.util.UrlStatusParameter.REG_SUCCESS;
-import static java.util.Objects.isNull;
 import static org.slf4j.LoggerFactory.getLogger;
 
 @SessionAttributes("account")
@@ -28,13 +27,12 @@ public class AuthController {
 
     private static final int EXPIRATION_COOKIE_LIFE_TIME = 0;
     private static final int REMEMBER_ME_COOKIE_LIFE_TIME = 3600;
-    private final LoginService loginService;
+//    private final LoginService loginService;
     private final PasswordService passwordService;
     private final AccountService accountService;
     private static final Logger logger = getLogger(AuthController.class);
 
-    public AuthController(LoginService loginService, PasswordService passwordService, AccountService accountService) {
-        this.loginService = loginService;
+    public AuthController(PasswordService passwordService, AccountService accountService) {
         this.passwordService = passwordService;
         this.accountService = accountService;
     }
@@ -44,25 +42,25 @@ public class AuthController {
         return "auth/login";
     }
 
-    @PostMapping("/login")
-    public String login(@RequestParam String email,
-                        @RequestParam String password,
-                        @RequestParam(required = false) String rememberMe,
-                        Model model,
-                        HttpServletResponse resp) {
-        Optional<Account> loggedInAccount = loginService.getLoggedInAccount(email, password);
-        if (loggedInAccount.isPresent()) {
-            Account account = loggedInAccount.get();
-            model.addAttribute("account", account);
-            if (!isNull(rememberMe)) {
-                createRememberMeCookie(account, resp);
-            }
-            logger.info("Account={} logged in successfully", account.getId());
-            return "redirect:/account?id=" + account.getId();
-        } else {
-            return "redirect:/login" + AUTH_DATA_ERROR.getValue();
-        }
-    }
+//    @PostMapping("/login")
+//    public String login(@RequestParam String email,
+//                        @RequestParam String password,
+//                        @RequestParam(required = false) String rememberMe,
+//                        Model model,
+//                        HttpServletResponse resp) {
+//        Optional<Account> loggedInAccount = loginService.getLoggedInAccount(email, password);
+//        if (loggedInAccount.isPresent()) {
+//            Account account = loggedInAccount.get();
+//            model.addAttribute("account", account);
+//            if (!isNull(rememberMe)) {
+//                createRememberMeCookie(account, resp);
+//            }
+//            logger.info("Account={} logged in successfully", account.getId());
+//            return "redirect:/account?id=" + account.getId();
+//        } else {
+//            return "redirect:/login" + AUTH_DATA_ERROR.getValue();
+//        }
+//    }
 
     private void createRememberMeCookie(Account account, HttpServletResponse resp) {
         resp.addCookie(createCookie("login", account.getEmail()));
