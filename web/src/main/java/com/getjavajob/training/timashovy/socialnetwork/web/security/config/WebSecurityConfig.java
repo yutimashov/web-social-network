e08aa8af14954d9321487916ffdc587ea73bf439
yutimashov <timashov.yuriy@gmail.com>
2024-12-18 16:27:03 +0300

    fix web security config: adapt to project requirments

--- web/src/main/java/com/getjavajob/training/timashovy/socialnetwork/web/security/config/WebSecurityConfig.java
+++ web/src/main/java/com/getjavajob/training/timashovy/socialnetwork/web/security/config/WebSecurityConfig.java
@@ -1,44 +1,60 @@
 package com.getjavajob.training.timashovy.socialnetwork.web.security.config;
 
+import com.getjavajob.training.timashovy.socialnetwork.web.security.config.service.AccountDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
 
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        return http
-//                .httpBasic().and()
-//                .authorizeHttpRequests()
-//                .anyRequest().authenticated()
-//                .and().build();
-//    }
+    @Autowired
+    private AccountDetailsService accountDetailsService;
 
-//    @Bean
-//    public UserDetailsService userDetailsService() {
-//        InMemoryUserDetailsManager userDetailsService = new InMemoryUserDetailsManager();
-//        UserDetails user = User.withUsername("user")
-//                .password(passwordEncoder().encode("password"))
-//                .roles("USER")
-//                .build();
-//        userDetailsService.createUser(user);
-//        return userDetailsService;
-//    }
-//
-//    @Bean
-//    public PasswordEncoder passwordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
-//
-//    @Bean
-//    public AuthenticationManager authManager(HttpSecurity http) throws Exception {
-//        return http.getSharedObject(AuthenticationManagerBuilder.class)
-//                .userDetailsService(userDetailsService())
-//                .passwordEncoder(passwordEncoder())
-//                .and()
-//                .build();
-//    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(
+                        registry -> {
+                            registry.requestMatchers("/account/all", "/register").permitAll();
+                            registry.requestMatchers("/group/**").hasRole("ADMIN");
+                            registry.requestMatchers("/account/**").hasRole("USER");
+                            registry.anyRequest().authenticated();
+                        }
+                )
+                .formLogin(AbstractAuthenticationFilterConfigurer::permitAll)
+                .logout(LogoutConfigurer::permitAll)
+                .csrf().disable()
+                .build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return accountDetailsService;
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService());
+        provider.setPasswordEncoder(passwordEncoder());
+        return provider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
 }
