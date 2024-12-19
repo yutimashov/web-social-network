ca0f2a3b532bd3b0583be2fdbb5e2831d5264a92
yutimashov <timashov.yuriy@gmail.com>
2024-12-19 16:22:41 +0300

    add logout functionality using spring security

--- web/src/main/java/com/getjavajob/training/timashovy/socialnetwork/web/controllers/AuthController.java
+++ web/src/main/java/com/getjavajob/training/timashovy/socialnetwork/web/controllers/AuthController.java
@@ -2,7 +2,6 @@ package com.getjavajob.training.timashovy.socialnetwork.web.controllers;
 
 import com.getjavajob.training.timashovy.socialnetwork.domain.account.Account;
 import com.getjavajob.training.timashovy.socialnetwork.service.interfaces.AccountService;
-import com.getjavajob.training.timashovy.socialnetwork.service.interfaces.PasswordService;
 import com.getjavajob.training.timashovy.socialnetwork.web.dto.AccountDto;
 import com.getjavajob.training.timashovy.socialnetwork.web.mappers.AccountMapper;
 import org.slf4j.Logger;
@@ -13,11 +12,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
 import static com.getjavajob.training.timashovy.socialnetwork.web.util.UrlStatusParameter.REG_SUCCESS;
 import static org.slf4j.LoggerFactory.getLogger;
 
@@ -25,8 +19,6 @@ import static org.slf4j.LoggerFactory.getLogger;
 @Controller
 public class AuthController {
 
-    private static final int EXPIRATION_COOKIE_LIFE_TIME = 0;
-    private static final int REMEMBER_ME_COOKIE_LIFE_TIME = 3600;
     private final AccountService accountService;
     private static final Logger logger = getLogger(AuthController.class);
 
@@ -38,40 +30,6 @@ public class AuthController {
     public String loginPage() {
         return "auth/login";
     }
-
-//    @PostMapping("/login")
-//    public String login(@RequestParam String email,
-//                        @RequestParam String password,
-//                        @RequestParam(required = false) String rememberMe,
-//                        Model model,
-//                        HttpServletResponse resp) {
-//        Optional<Account> loggedInAccount = loginService.getLoggedInAccount(email, password);
-//        if (loggedInAccount.isPresent()) {
-//            Account account = loggedInAccount.get();
-//            model.addAttribute("account", account);
-//            if (!isNull(rememberMe)) {
-//                createRememberMeCookie(account, resp);
-//            }
-//            logger.info("Account={} logged in successfully", account.getId());
-//            return "redirect:/account?id=" + account.getId();
-//        } else {
-//            return "redirect:/login" + AUTH_DATA_ERROR.getValue();
-//        }
-//    }
-//
-//    private void createRememberMeCookie(Account account, HttpServletResponse resp) {
-//        resp.addCookie(createCookie("login", account.getEmail()));
-//        if (passwordService.get(account.getId()).isPresent()) {
-//            resp.addCookie(createCookie("password", passwordService.get(account.getId()).get()
-//                    .getPasswordValue()));
-//        }
-//    }
-//
-//    private Cookie createCookie(String cookieName, String cookieValue) {
-//        Cookie cookie = new Cookie(cookieName, cookieValue);
-//        cookie.setMaxAge(REMEMBER_ME_COOKIE_LIFE_TIME);
-//        return cookie;
-//    }
 
     @GetMapping("/register")
     public String getRegisterPage() {
@@ -87,29 +45,6 @@ public class AuthController {
                 workingPhones);
         logger.info("New account={} has been registered", account.getId());
         return "redirect:/login" + REG_SUCCESS.getValue();
-    }
-
-    @GetMapping("/logout")
-    public String logoutPage(HttpSession session, HttpServletRequest req, HttpServletResponse resp) {
-        session.invalidate();
-        clearAllCookies(req, resp);
-        return "redirect:/login";
-    }
-
-    private void clearAllCookies(HttpServletRequest req, HttpServletResponse resp) {
-        Cookie[] cookies = req.getCookies();
-        if (cookies != null) {
-            for (Cookie cookie : cookies) {
-                resp.addCookie(clearCookie(cookie));
-            }
-        }
-    }
-
-    private Cookie clearCookie(Cookie cookie) {
-        cookie.setMaxAge(EXPIRATION_COOKIE_LIFE_TIME);
-        cookie.setValue(null);
-        cookie.setPath("/");
-        return cookie;
     }
 
 }
--- web/src/main/java/com/getjavajob/training/timashovy/socialnetwork/web/security/config/WebSecurityConfig.java
+++ web/src/main/java/com/getjavajob/training/timashovy/socialnetwork/web/security/config/WebSecurityConfig.java
@@ -8,7 +8,6 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -50,9 +49,15 @@ public class WebSecurityConfig {
                             .failureUrl("/login" + AUTH_DATA_ERROR.getValue())
                             .permitAll();
                 })
-                .logout(LogoutConfigurer::permitAll)
-                .csrf().disable()
-                .rememberMe().userDetailsService(accountDetailsService).tokenValiditySeconds(86400)
+                .logout()
+                    .invalidateHttpSession(true)
+                    .deleteCookies("JSESSIONID")
+                .and()
+                .csrf()
+                    .disable()
+                .rememberMe()
+                    .userDetailsService(accountDetailsService)
+                    .tokenValiditySeconds(86400)
                 .and()
                 .build();
     }
