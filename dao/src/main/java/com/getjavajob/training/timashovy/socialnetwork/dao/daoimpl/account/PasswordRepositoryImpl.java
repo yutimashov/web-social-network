bb69afacb9be7124fa471094f43d7b7e46968e8e
yutimashov <timashov.yuriy@gmail.com>
2024-12-27 11:18:27 +0300

    rewrite PasswordRepository using Spring Data

--- dao/src/main/java/com/getjavajob/training/timashovy/socialnetwork/dao/daoimpl/account/PasswordRepositoryImpl.java
+++ dao/src/main/java/com/getjavajob/training/timashovy/socialnetwork/dao/daoimpl/account/PasswordRepositoryImpl.java
@@ -1,20 +1,13 @@
 package com.getjavajob.training.timashovy.socialnetwork.dao.daoimpl.account;
 
-import com.getjavajob.training.timashovy.socialnetwork.dao.exception.DaoException;
 import com.getjavajob.training.timashovy.socialnetwork.dao.interfaces.account.PasswordRepository;
+import com.getjavajob.training.timashovy.socialnetwork.dao.interfaces.account.PasswordRepositorySpringData;
 import com.getjavajob.training.timashovy.socialnetwork.domain.password.Password;
 import org.slf4j.Logger;
 import org.springframework.stereotype.Repository;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.NoResultException;
-import jakarta.persistence.PersistenceContext;
-import jakarta.persistence.PersistenceException;
 import java.util.Optional;
 
-import static java.util.Objects.isNull;
-import static java.util.Optional.empty;
-import static java.util.Optional.ofNullable;
 import static org.slf4j.LoggerFactory.getLogger;
 
 /**
@@ -26,58 +19,30 @@ public class PasswordRepositoryImpl implements PasswordRepository {
 
     private static final Logger logger = getLogger(PasswordRepositoryImpl.class);
 
-    @PersistenceContext
-    private EntityManager entityManager;
+    private final PasswordRepositorySpringData passwordRepositorySpringData;
+
+    public PasswordRepositoryImpl(PasswordRepositorySpringData passwordRepositorySpringData) {
+        this.passwordRepositorySpringData = passwordRepositorySpringData;
+    }
 
     @Override
     public Password save(Password password) {
-        try {
-            entityManager.persist(password);
-            return password;
-        } catch (PersistenceException e) {
-            logger.error("Error persisting password for accountId={}", password.getId());
-            throw new DaoException("Cannot save password to persistent storage", e);
-        }
+        return passwordRepositorySpringData.save(password);
     }
 
     @Override
     public Optional<Password> getById(Long id) {
-        try {
-            return ofNullable(entityManager.find(Password.class, id));
-        } catch (PersistenceException e) {
-            logger.error("Error getting password by id: id={}", id);
-            throw new DaoException("Cannot get password by provided id", e);
-        }
+        return passwordRepositorySpringData.findById(id);
     }
 
     @Override
     public Optional<Password> findByEmail(String email) {
-        try {
-            return ofNullable(entityManager.createQuery(
-                            "select p from Password p join p.account a where a.email = :email",
-                            Password.class)
-                    .setParameter("email", email)
-                    .getSingleResult()
-            );
-        } catch (NoResultException e) {
-            return empty();
-        } catch (PersistenceException e) {
-            logger.error("Error getting password by email: email={}", email);
-            throw new DaoException("Cannot get password by provided email", e);
-        }
+        return passwordRepositorySpringData.findByAccountEmail(email);
     }
 
     @Override
     public void delete(Long id) {
-        try {
-            Password existingPassword = entityManager.find(Password.class, id);
-            if (!isNull(existingPassword)) {
-                entityManager.remove(existingPassword);
-            }
-        } catch (PersistenceException e) {
-            logger.error("Error deleting password with id={}", id);
-            throw new DaoException("Cannot delete password by provided id", e);
-        }
+        passwordRepositorySpringData.deleteById(id);
     }
 
 }
---
+++ dao/src/main/java/com/getjavajob/training/timashovy/socialnetwork/dao/interfaces/account/PasswordRepositorySpringData.java
@@ -0,0 +1,12 @@
+package com.getjavajob.training.timashovy.socialnetwork.dao.interfaces.account;
+
+import com.getjavajob.training.timashovy.socialnetwork.domain.password.Password;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface PasswordRepositorySpringData extends CrudRepository<Password, Long> {
+
+    Optional<Password> findByAccountEmail(String email);
+
+}
