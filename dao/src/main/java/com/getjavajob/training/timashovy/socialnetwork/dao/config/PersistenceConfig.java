8ac919cc3b66abc50d0a219c765ea3d8a1d2cd35
yutimashov <timashov.yuriy@gmail.com>
2024-12-29 00:12:15 +0300

    remove unnecessary @EnableTransactionManagement dependency

--- dao/src/main/java/com/getjavajob/training/timashovy/socialnetwork/dao/config/PersistenceConfig.java
+++ dao/src/main/java/com/getjavajob/training/timashovy/socialnetwork/dao/config/PersistenceConfig.java
@@ -3,10 +3,8 @@ package com.getjavajob.training.timashovy.socialnetwork.dao.config;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
-@EnableTransactionManagement
 @EnableJpaRepositories(basePackages = {"com.getjavajob.training.timashovy.socialnetwork.dao.interfaces"})
 @EntityScan(basePackages = {"com.getjavajob.training.timashovy.socialnetwork.domain"})
 public class PersistenceConfig {
