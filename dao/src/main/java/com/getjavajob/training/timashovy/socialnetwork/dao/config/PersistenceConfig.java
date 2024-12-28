88406dca78356431e74af57dd797a329fbcb8c13
yutimashov <timashov.yuriy@gmail.com>
2024-12-29 00:07:29 +0300

    extract data from persistance config to application properties

--- dao/src/main/java/com/getjavajob/training/timashovy/socialnetwork/dao/config/PersistenceConfig.java
+++ dao/src/main/java/com/getjavajob/training/timashovy/socialnetwork/dao/config/PersistenceConfig.java
@@ -1,49 +1,13 @@
 package com.getjavajob.training.timashovy.socialnetwork.dao.config;
 
-import jakarta.persistence.EntityManagerFactory;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-import javax.sql.DataSource;
 
 @Configuration
 @EnableTransactionManagement
-@EnableJpaRepositories(basePackages = "com.getjavajob.training.timashovy.socialnetwork.dao.interfaces")
+@EnableJpaRepositories(basePackages = {"com.getjavajob.training.timashovy.socialnetwork.dao.interfaces"})
+@EntityScan(basePackages = {"com.getjavajob.training.timashovy.socialnetwork.domain"})
 public class PersistenceConfig {
-
-    @Bean
-    public DataSource dataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setUrl("jdbc:postgresql://localhost:5432/social_network");
-        dataSource.setUsername("postgres");
-        dataSource.setPassword("21122012loliwe+");
-        return dataSource;
-    }
-
-
-    @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
-        LocalContainerEntityManagerFactoryBean entityManagerFactory = new LocalContainerEntityManagerFactoryBean();
-        entityManagerFactory.setDataSource(dataSource);
-        entityManagerFactory.setPackagesToScan("com.getjavajob.training.timashovy.socialnetwork.domain");
-        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-        vendorAdapter.setShowSql(true);
-        vendorAdapter.setGenerateDdl(false);
-        vendorAdapter.setDatabasePlatform("org.hibernate.dialect.PostgreSQLDialect");
-        entityManagerFactory.setJpaVendorAdapter(vendorAdapter);
-        return entityManagerFactory;
-    }
-
-    @Bean
-    public JpaTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
-        return new JpaTransactionManager(entityManagerFactory);
-    }
-
 }
