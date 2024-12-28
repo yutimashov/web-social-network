e806ca6d2c147c9c6c58d80e9c610ede7dd62e01
yutimashov <timashov.yuriy@gmail.com>
2024-12-28 23:54:35 +0300

    extract functions of scanning packages to web config

--- web/src/main/java/com/getjavajob/training/timashovy/socialnetwork/web/ApplicationRunner.java
+++ web/src/main/java/com/getjavajob/training/timashovy/socialnetwork/web/ApplicationRunner.java
@@ -4,14 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {
-        "com.getjavajob.training.timashovy.socialnetwork.web",
-        "com.getjavajob.training.timashovy.socialnetwork.service",
-        "com.getjavajob.training.timashovy.socialnetwork.dao"
-})
 public class ApplicationRunner extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
--- web/src/main/java/com/getjavajob/training/timashovy/socialnetwork/web/config/WebConfig.java
+++ web/src/main/java/com/getjavajob/training/timashovy/socialnetwork/web/config/WebConfig.java
@@ -1,10 +1,15 @@
 package com.getjavajob.training.timashovy.socialnetwork.web.config;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@ComponentScan(basePackages = {
+        "com.getjavajob.training.timashovy.socialnetwork.service",
+        "com.getjavajob.training.timashovy.socialnetwork.dao"
+})
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
