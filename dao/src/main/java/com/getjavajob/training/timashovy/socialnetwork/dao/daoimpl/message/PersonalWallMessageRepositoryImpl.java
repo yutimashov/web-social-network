4eeba7543ea190cb2b9b06023bd2407148c622c7
yutimashov <timashov.yuriy@gmail.com>
2024-12-29 10:39:17 +0300

    rewrite PersonalWallMessageRepositoryImpl using Spring Data

--- dao/src/main/java/com/getjavajob/training/timashovy/socialnetwork/dao/daoimpl/message/PersonalWallMessageRepositoryImpl.java
+++ dao/src/main/java/com/getjavajob/training/timashovy/socialnetwork/dao/daoimpl/message/PersonalWallMessageRepositoryImpl.java
@@ -1,61 +1,42 @@
 package com.getjavajob.training.timashovy.socialnetwork.dao.daoimpl.message;
 
 import com.getjavajob.training.timashovy.socialnetwork.dao.interfaces.repositories.message.PersonalWallMessageRepository;
+import com.getjavajob.training.timashovy.socialnetwork.dao.interfaces.springdatarepositories.PersonalWallMessageRepositorySpringData;
 import com.getjavajob.training.timashovy.socialnetwork.domain.message.PersonalWallMessage;
 import org.springframework.stereotype.Repository;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.NoResultException;
-import jakarta.persistence.PersistenceContext;
 import java.util.List;
 import java.util.Optional;
-
-import static java.util.Objects.isNull;
-import static java.util.Optional.empty;
-import static java.util.Optional.ofNullable;
 
 @Repository
 public class PersonalWallMessageRepositoryImpl implements PersonalWallMessageRepository {
 
-    @PersistenceContext
-    private EntityManager entityManager;
+    private final PersonalWallMessageRepositorySpringData personalWallMessageRepositorySpringData;
+
+    public PersonalWallMessageRepositoryImpl(PersonalWallMessageRepositorySpringData personalWallMessageRepositorySpringData) {
+        this.personalWallMessageRepositorySpringData = personalWallMessageRepositorySpringData;
+    }
 
     @Override
     public PersonalWallMessage save(PersonalWallMessage personalMessage) {
-        entityManager.persist(personalMessage);
+        personalWallMessageRepositorySpringData.save(personalMessage);
         return personalMessage;
     }
 
     @Override
     public Optional<PersonalWallMessage> getById(Long id) {
-        try {
-            PersonalWallMessage existingMessage = entityManager.createQuery(
-                            "select pm from PersonalWallMessage pm where pm.id = :messageId",
-                            PersonalWallMessage.class)
-                    .setParameter("messageId", id)
-                    .getSingleResult();
-            return ofNullable(existingMessage);
-        } catch (NoResultException e) {
-            return empty();
-        }
+        return personalWallMessageRepositorySpringData.findById(id);
     }
 
     @Override
     public List<PersonalWallMessage> getAll(Long accountId) {
-        return entityManager.createQuery(
-                        "select pm from PersonalWallMessage pm where pm.accountReceiverId = :accountId "
-                                + "order by pm.creationDate desc",
-                        PersonalWallMessage.class
-                )
-                .setParameter("accountId", accountId)
-                .getResultList();
+        return personalWallMessageRepositorySpringData.findAllByAccountReceiverIdOrderByCreationDateDesc(accountId);
     }
 
     @Override
     public void delete(Long id) {
-        PersonalWallMessage existingPersonalWallMessage = entityManager.find(PersonalWallMessage.class, id);
-        if (!isNull(existingPersonalWallMessage)) {
-            entityManager.remove(existingPersonalWallMessage);
+        if (personalWallMessageRepositorySpringData.existsById(id)) {
+            personalWallMessageRepositorySpringData.deleteById(id);
         }
     }
 
---
+++ dao/src/main/java/com/getjavajob/training/timashovy/socialnetwork/dao/interfaces/springdatarepositories/PersonalWallMessageRepositorySpringData.java
@@ -0,0 +1,17 @@
+package com.getjavajob.training.timashovy.socialnetwork.dao.interfaces.springdatarepositories;
+
+import com.getjavajob.training.timashovy.socialnetwork.domain.message.PersonalWallMessage;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface PersonalWallMessageRepositorySpringData extends CrudRepository<PersonalWallMessage, Long> {
+
+    @Query("""
+            select pm from PersonalWallMessage pm where pm.accountReceiverId = :accountId order by pm.creationDate desc
+            """)
+    List<PersonalWallMessage> findAllByAccountReceiverIdOrderByCreationDateDesc(@Param("accountId") Long accountId);
+
+}
