ebd220fba33c4e9603a78fa69762e54b098224ff
yutimashov <timashov.yuriy@gmail.com>
2025-01-31 11:22:45 +0300

    refactor query syntax

--- dao/src/main/java/com/getjavajob/training/timashovy/socialnetwork/dao/interfaces/springdatarepositories/friendship/FriendshipRepositorySpringData.java
+++ dao/src/main/java/com/getjavajob/training/timashovy/socialnetwork/dao/interfaces/springdatarepositories/friendship/FriendshipRepositorySpringData.java
@@ -17,34 +17,45 @@ public interface FriendshipRepositorySpringData extends CrudRepository<Friendshi
     @Transactional
     @Modifying
     @Query("""
-            update Friendship f set f.friendshipStatus = true where f.requester.id = :requester
-            and f.receiver.id = :accepter
+                update Friendship f
+                set f.friendshipStatus = true
+                where f.requester.id = :requester
+                and f.receiver.id = :accepter
             """)
     int acceptRequest(@Param("requester") Long requesterId, @Param("accepter") Long accepterId);
 
     @Query("""
-            select a
+                select a
                 from Account a
                 join Friendship f on (f.initiatorAccountId = a.id or f.friendAccountId = a.id)
-                where (f.initiatorAccountId = :accountId OR f.friendAccountId = :accountId)
+                where (f.initiatorAccountId = :accountId or f.friendAccountId = :accountId)
                 and a.id != :accountId
                 and f.friendshipStatus = true
             """)
     Page<Account> getFriends(@Param("accountId") Long accountId, Pageable page);
 
     @Query("""
-            select f.requester.id from Friendship f where f.friendshipStatus = false and f.receiver.id = :accountId
+                select f.requester.id
+                from Friendship f
+                where f.friendshipStatus = false
+                and f.receiver.id = :accountId
             """)
     List<Long> getIncomingRequests(@Param("accountId") Long accountId);
 
-    @Query("select f.receiver.id from Friendship f where f.friendshipStatus = false and f.requester.id = :accountId")
+    @Query("""
+                select f.receiver.id
+                from Friendship f
+                where f.friendshipStatus = false and f.requester.id = :accountId
+            """)
     List<Long> getOutgoingRequests(@Param("accountId") Long accountId);
 
     @Transactional
     @Modifying
     @Query("""
-            delete from Friendship f where (f.initiatorAccountId = :accountId and f.friendAccountId = :deletingFriendId)
-            or (f.initiatorAccountId = :deletingFriendId and f.friendAccountId = :accountId)
+                delete from Friendship f
+                where (f.initiatorAccountId = :accountId
+                and f.friendAccountId = :deletingFriendId)
+                or (f.initiatorAccountId = :deletingFriendId and f.friendAccountId = :accountId)
             """)
     void deleteFriend(@Param("accountId") Long accountId, @Param("deletingFriendId") Long deletingFriendId);
 
