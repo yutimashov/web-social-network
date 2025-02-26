698e3857bfafa8739aad9f33546724a46014425a
yutimashov <timashov.yuriy@gmail.com>
2025-02-26 20:49:30 +0300

    add rabbitmq config

---
+++ service/src/main/java/com/getjavajob/training/timashovy/socialnetwork/service/config/RabbitMQConfig.java
@@ -0,0 +1,41 @@
+package com.getjavajob.training.timashovy.socialnetwork.service.config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    @Value("${rabbit.mq.queue.name}")
+    private String queueName;
+
+    @Value("${rabbit.mq.exchange.name}")
+    private String exchangeName;
+
+    @Value("${rabbit.mq.routing.key}")
+    private String routingKey;
+
+    @Bean
+    public Queue queue() {
+        return new Queue(queueName, true);
+    }
+
+    @Bean
+    public TopicExchange exchange() {
+        return new TopicExchange(exchangeName, true, false);
+    }
+
+    @Bean
+    public Binding binding(Queue queue, TopicExchange exchange) {
+        return BindingBuilder
+                .bind(queue)
+                .to(exchange)
+                .with(routingKey);
+    }
+
+}
