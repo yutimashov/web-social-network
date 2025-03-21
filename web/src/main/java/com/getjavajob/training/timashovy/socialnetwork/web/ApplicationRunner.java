613c4c7a79391fcf147f89af69b6d62ae1fb208e
yutimashov <timashov.yuriy@gmail.com>
2025-03-21 13:42:34 +0300

    add enabling caching

--- web/src/main/java/com/getjavajob/training/timashovy/socialnetwork/web/ApplicationRunner.java
+++ web/src/main/java/com/getjavajob/training/timashovy/socialnetwork/web/ApplicationRunner.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableCaching
 public class ApplicationRunner extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
