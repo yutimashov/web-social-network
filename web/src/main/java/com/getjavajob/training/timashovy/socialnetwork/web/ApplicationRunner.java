2c2a5f83db099cc148f3e224028a8e7daaf37caf
yutimashov <timashov.yuriy@gmail.com>
2025-04-06 10:08:50 +0300

    extract hardcoded values for cahing into properties file

--- web/src/main/java/com/getjavajob/training/timashovy/socialnetwork/web/ApplicationRunner.java
+++ web/src/main/java/com/getjavajob/training/timashovy/socialnetwork/web/ApplicationRunner.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
