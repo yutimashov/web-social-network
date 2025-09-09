1653121adb862a565f2a26cfc53dd98c8bc516b2
yutimashov <timashov.yuriy@gmail.com>
2025-09-09 08:47:58 +0300

    make friedndship api controller restcoller

--- friendship-service/src/main/java/com/getjavajob/friendshipservice/web/api/FriendshipApiController.java
+++ friendship-service/src/main/java/com/getjavajob/friendshipservice/web/api/FriendshipApiController.java
@@ -2,14 +2,14 @@ package com.getjavajob.friendshipservice.web.api;
 
 import com.getjavajob.friendshipservice.service.FriendshipCheckerService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import static org.springframework.http.HttpStatus.OK;
 
-@Controller
+@RestController
 @RequestMapping("/api/friendship")
 public class FriendshipApiController {
 
